6fcf3aac2cc021a2e6ec92502b2019433381db02
AroundTheCorner <55585693+Athene1337@users.noreply.github.com>
2022-09-01 10:59:01 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title="Excel Plotter")
+st.title("Excel Plotter")
+st.subheader("I'm hungry, feed me an Excel file!")
