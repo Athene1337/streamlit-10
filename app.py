970ce54d150b077fafa5fddc718ea3f3549bb635
AroundTheCorner <55585693+Athene1337@users.noreply.github.com>
2022-09-01 11:20:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ st.subheader("I'm hungry, feed me an Excel file!")
 ExcelFile = st.file_uploader('What is on the menu?', type='xlsx')
 if ExcelFile:
   st.markdown('---')
-  df = pd.read_excel(uploaded_file, engine='openpyxl')
+  df = pd.read_excel(ExcelFile, engine='openpyxl')
   st.dataframe
 
